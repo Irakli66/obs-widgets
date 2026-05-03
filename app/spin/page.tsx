00d946f1f11c9ac515783ad0ec2e34d421116c48
irakli66 <xarshiladzeirakli@gmail.com>
2026-05-03 18:42:19 +0400

    remove: overlay for spin page

--- app/spin/page.tsx
+++ app/spin/page.tsx
@@ -5,7 +5,7 @@ import SpinWheel from "../components/spin/SpinWheel";
 
 export default function SpinPage() {
   return (
-    <div className="min-h-screen bg-black/45 flex items-center justify-center px-6 backdrop-blur-[1px]">
+    <div className="min-h-screen bg-transparent flex items-center justify-center px-6">
       <SpinListener />
       <SpinWheel />
     </div>
